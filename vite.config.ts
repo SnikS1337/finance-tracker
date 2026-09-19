fc32f27b5424446c5af444154803c2f2d7e9711c
SnikS1337 <35911914+SnikS1337@users.noreply.github.com>
2026-09-19 13:40:46 +0300

    fix: use root base path on Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import { VitePWA } from 'vite-plugin-pwa'
 
-const base = '/finance-tracker/'
+const base = process.env.VERCEL === '1' ? '/' : '/finance-tracker/'
 
 export default defineConfig({
   base,
